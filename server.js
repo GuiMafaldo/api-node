ab89c1d9eb11a7f2d70b7eca1785e2b880a0288d
Guilherme <thon_firmesao@gmail.com>
2024-06-01 13:33:08 -0300

    fix

--- server.js
+++ server.js
@@ -7,12 +7,6 @@ const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Permitindo todas as origens. Ajuste conforme necessário.
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
 
 app.post('/colaboradores', async (req, res) => {
     try {
